27954a634deab7e1017d112a9ab20dbb0fe6c64f
Rok Mlinar Vahtar <rokmlinar.p@gmail.com>
2025-02-26 17:31:34 +0100

    1

---
+++ Naloga1/solver.py
@@ -0,0 +1,84 @@
+import numpy as np
+import scipy
+import matplotlib.pyplot as plt
+from matplotlib import cm, colors
+import scipy.special
+from tqdm import tqdm
+import re, sys
+
+from scipy.linalg import solve
+
+np.set_printoptions(linewidth=10000, threshold=sys.maxsize)
+
+def basedwise_scalar_product(i, state, coefs):
+    mid = int(len(coefs)//2)
+    val = 0
+    for k in range(len(coefs)):
+        try:
+            val += state[i-mid+k] * coefs[k]
+        except IndexError:
+            pass
+    
+    return val
+
+
+
+def finite_differencer(startstate, V, tau, h, nmax, spacewise = [1/2, -1, 1/2]):
+    state = np.zeros((len(startstate), nmax), dtype=np.float64)
+    state[:,0] = startstate
+    for n in range(1,nmax,1):
+        for m in range(len(startstate)):
+            state[m,n] = state[m,n-1] + 1j*tau * (1/h**2 * basedwise_scalar_product(m, state[:,n-1], spacewise) - V[m,n])
+
+    return state
+
+
+def finite_propagatorer(startstate, V, tau, h, nmax, spacewise = [1/2, -1, 1/2]):
+    K = 10
+
+    HmatrixP = np.zeros((len(startstate), len(startstate)), dtype=np.float64)
+    ###momentum part
+    for i in range(len(spacewise)):
+        mid = int(len(spacewise)//2)
+        HmatrixP += -1/2 * 1/h**2 * np.diag(spacewise[i], k=i-mid)
+
+    state = np.zeros((len(startstate), nmax), dtype=np.float64)
+    state[:,0] = startstate
+
+    for n in range(1,nmax,1):
+        for k in range(0,K+1,1):
+            #Potential part
+            HmatrixV = np.diag(V[:,n])
+            Hmatrix = HmatrixP + HmatrixV
+
+            state[:,n] += (-1j * tau)**k/scipy.special.factorial(k) * np.linalg.matrix_power(Hmatrix, k).dot(state[:,n-1])
+
+    return state
+
+def implicinator(startstate, V, tau, h, nmax, spacewise = [1/2, -1, 1/2]):
+    HmatrixP = np.zeros((len(startstate), len(startstate)), dtype=np.float64)
+    ###momentum part
+    for i in range(len(spacewise)):
+        mid = int(len(spacewise)//2)
+        HmatrixP += -1/2 * 1/h**2 * np.diag(spacewise[i], k=i-mid)
+
+    state = np.zeros((len(startstate), nmax), dtype=np.float64)
+    state[:,0] = startstate
+
+    for n in range(1,nmax,1):
+        #Potential part
+        HmatrixV = np.diag(V[:,n])
+        Hmatrix = HmatrixP + HmatrixV
+
+
+        b = (np.eye(len(startstate)) - 1j*tau/2 * Hmatrix).dot(state[:,n-1])
+
+        A = (np.eye(len(startstate)) + 1j*tau/2 * Hmatrix)
+
+        state[:,n] = solve(A,b,assume_a="banded")
+
+    return state
+
+
+
+
