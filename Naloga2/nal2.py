4fcdfcd035be4e535241dc2975fc576ca10c36e3
Rok Mlinar Vahtar <rokmlinar.p@gmail.com>
2025-03-05 18:52:02 +0100

    a

--- Naloga2/nal2.py
+++ Naloga2/nal2.py
@@ -8,71 +8,153 @@ import scipy.special
 from tqdm import tqdm
 import re, sys
 from matplotlib.animation import FuncAnimation
+import time
 
 
-def eigenstate(N, x):
-    return 1/(np.pi**(1/4) * np.sqrt(2**N * scipy.special.factorial(N))) * scipy.special.hermite(N)(x) * np.exp(-x**2/2)
+from solver import mat_v_bazi22 as basic, mat_v_bazi2 as basic_herm, Lanczos_method as lanczos, eigenstate
 
 
-def mat_element(n1,n2,f):
-    xs = np.linspace(-20,20,1000)
-    ys = eigenstate(n1,xs) * f(xs) * eigenstate(n2,xs)
 
-    return np.trapz(ys,xs)
 
-def mat_element_fsymmetric(n1,n2,f):
-    if np.logical_xor(n1 % 2 == 1, n2 % 2 == 1): return 0
-    xs = np.linspace(-20,20,1000)
-    ys = eigenstate(n1,xs) * f(xs) * eigenstate(n2,xs)
 
-    return np.trapz(ys,xs)
+"""Nmaxs = np.int32(10**np.linspace(0,2.1,10))[::-1] + 4
+t_basic, t_basic_h, t_lancz_0, t_lancz_r = [], [], [], []
 
-def mat_element_fantisymmetric(n1,n2,f):
-    if (n1 % 2 == 1) == (n2 % 2 == 1): return 0
-    xs = np.linspace(-20,20,1000)
-    ys = eigenstate(n1,xs) * f(xs) * eigenstate(n2,xs)
+for i in tqdm(range(len(Nmaxs))):
+    Nmax = Nmaxs[i]
 
-    return np.trapz(ys,xs)
+    stime = time.time()
+    basic(Nmax,1)
+    t_basic.append(time.time()-stime)
 
-def mat_v_bazi(Nmax, lamb):
-    Es = np.array([n + 1/2 + lamb * mat_element(n,n,lambda x:x**4) for n in range(Nmax)], dtype=complex)
-    H = np.diag(Es)
+    stime = time.time()
+    basic_herm(Nmax,1)
+    t_basic_h.append(time.time()-stime)
 
-    for i in range(Nmax):
-        for j in range(i):
-            print(f"{i}, {j}")
-            H[i,j] += lamb * mat_element(i,j,lambda x:x**4)
-            H[j,i] = np.conj(H[i,j])
 
-    eigvals, eigvects = np.linalg.eigh(H)
+    psi0 = np.zeros(Nmax)
+    psi0[0] = 1
+    stime = time.time()
+    lanczos(psi0,Nmax,1)
+    t_lancz_0.append(time.time()-stime)
 
-    return eigvals, eigvects
+    psi0 = np.random.random(Nmax)
+    psi0 = psi0/np.linalg.norm(psi0)
+    stime = time.time()
+    lanczos(psi0,Nmax,1)
+    t_lancz_r.append(time.time()-stime)
 
-def mat_v_bazi2(Nmax, lamb): #Če je H' simetričen
-    Es = np.array([n + 1/2 + lamb * mat_element_fsymmetric(n,n,lambda x:x**4) for n in range(Nmax)], dtype=complex)
-    H = np.diag(Es)
 
-    for i in range(Nmax):
-        for j in range(i):
-            print(f"{i}, {j}")
-            H[i,j] += lamb * mat_element_fsymmetric(i,j,lambda x:x**4)
-            H[j,i] = np.conj(H[i,j])
+plt.plot(Nmaxs, t_basic, label = "scipy.linalg.eig")
+plt.plot(Nmaxs, t_basic_h, label = "scipy.linalg.eigh")
+plt.plot(Nmaxs, t_lancz_0, label = "Lanzcos 0")
+plt.plot(Nmaxs, t_lancz_r, label = "Lanzcos random")
 
-    eigvals, eigvects = np.linalg.eigh(H)
+plt.legend()
+plt.xlabel("$N$")
+plt.ylabel("Čas računanja")
+plt.xscale("log")
+plt.yscale("log")
+plt.show()"""
 
-    return eigvals, eigvects
 
-def mat_v_bazi3(Nmax, lamb): #Če je H' antisimetričen
-    Es = np.array([n + 1/2 + lamb * mat_element_fantisymmetric(n,n,lambda x:x**4) for n in range(Nmax)], dtype=complex)
-    H = np.diag(Es)
+"""Nmax = 100
+lamb = 0.01
 
-    for i in range(Nmax):
-        for j in range(i):
-            print(f"{i}, {j}")
-            H[i,j] += lamb * mat_element_fantisymmetric(i,j,lambda x:x**3)
-            H[j,i] = np.conj(H[i,j])
+es, fis = basic(Nmax,lamb)
+plt.scatter([i for i in range(len(es))], es[::-1], label = "scipy.linalg.eig")
+es, fis = basic_herm(Nmax,lamb)
+plt.scatter([i for i in range(len(es))], es, label = "scipy.linalg.eigh")
 
-    eigvals, eigvects = np.linalg.eigh(H)
 
-    return eigvals, eigvects
+
+psi0 = np.zeros(Nmax)
+psi0[0] = 1
+es, fis = lanczos(psi0,Nmax,lamb)
+plt.scatter([i for i in range(len(es))], es, label = "Lanzcos 0")
+
+psi0 = np.random.random(Nmax)
+psi0 = psi0/np.linalg.norm(psi0)
+es, fis = lanczos(psi0,Nmax,lamb)
+plt.scatter([i for i in range(len(es))], es, label = "Lanzcos random")
+
+plt.legend()
+plt.xlabel("Zaporedno št. stanja")
+plt.ylabel("Energija")
+plt.yscale("log")
+plt.show()
+"""
+
+
+"""Nmaxs = np.arange(10,200,20)
+
+lamb = 1
+
+for i in tqdm(range(len(Nmaxs))):
+    Nmax = Nmaxs[i]
+    es, fis = basic_herm(Nmax,lamb)
+    plt.scatter([i for i in range(len(es))], es, label = f"$N = {Nmax}$", s = 3)
+
+plt.legend()
+plt.xlabel("Zaporedno št. stanja")
+plt.ylabel("Energija")
+plt.yscale("log")
+plt.title(f"$\\lambda = {lamb}$")
+plt.show()"""
+
+
+Nmax = 200
+
+lambs = [0,0.1,0.5,1,2]
+
+for i in tqdm(range(len(lambs))):
+    lamb = lambs[i]
+    es, fis = basic_herm(Nmax,lamb)
+    #plt.scatter([i for i in range(len(es))], es, label = f"$\\lambda = {lamb}$", s = 3)
+    plt.scatter([i for i in range(len(es))], es, label = f"$\\lambda = {lamb}$", s = 6)
+
+plt.legend()
+plt.xlabel("Zaporedno št. stanja")
+plt.ylabel("Energija")
+plt.yscale("log")
+
+plt.show()
+
+
+
+
+"""def is_it_in(lamb,E):
+    xmax = np.sqrt(np.sqrt(1/(16*lamb**2)+E/lamb)-1/(4*lamb))
+    xs = np.linspace(0,xmax,100)
+    ps = np.sqrt(2*E - xs**2 - 2*lamb*xs**4)
+
+    rs2 = xs**2 + ps**2
+
+    truths = rs2 > 2*E
+
+    if int(np.sum(truths)) != 0:
+        return False
+    return True
+
+def max_fittable_E(lamb):
+    Emin = 0
+    Emax = 10**6
+
+    while(Emax - Emin > 0.1):
+        Emid = Emin/2 + Emax/2
+        val = is_it_in(lamb,Emid)
+        if val:
+            Emin = Emid
+        else:
+            Emax = Emid
+
+    return Emid
+
+def mu_surface(lamb, E):
+    xmax = np.sqrt(np.sqrt(1/(16*lamb**2)+E/lamb)-1/(4*lamb))
+    xs = np.linspace(0,xmax,500)
+
+    return 4 * np.trapz(np.sqrt(2*E - xs**2 - 2*lamb*xs**4), xs)
+"""
+
 
